1b7454fb695b04a4549ca655137949879c6ba1e5
LombardiniJere <jere.lombardini@gmail.com>
2024-10-04 09:48:58 -0300

    adding a set of exercises and adding sets to each exercise

--- app/(screens)/createDayActivityScreen.tsx
+++ app/(screens)/createDayActivityScreen.tsx
@@ -18,6 +18,7 @@ import SaveButton from "../../components/navigation/SaveButton";
 import CancelButton from "../../components/navigation/CancelButton";
 import ButtonSecondary from "../../components/buttons/ButtonSecondary";
 import { Exercise } from "../../interfaces/Exercise.interfaces";
+import ButtonPrimary from "../../components/buttons/ButtonPrimary";
 
 const saveDayActivitiesToStorage = async (dayActivities) => {
   try {
@@ -38,10 +39,62 @@ export default function CreateDayActivity() {
   const router = useRouter();
   const { selectedExercises: selectedExercisesString } = useLocalSearchParams();
 
-  const selectedExercises =
-    typeof selectedExercisesString === "string"
-      ? JSON.parse(selectedExercisesString)
-      : selectedExercisesString;
+  // const selectedExercises =
+  //   typeof selectedExercisesString === "string"
+  //     ? JSON.parse(selectedExercisesString)
+  //     : selectedExercisesString;
+
+  const [selectedExercises, setSelectedExercises] = useState(() => {
+    // Check if selectedExercisesString is a string, then parse it
+    let exercisesArray: Exercise[] = [];
+
+    if (typeof selectedExercisesString === "string") {
+      try {
+        exercisesArray = JSON.parse(selectedExercisesString);
+      } catch (error) {
+        console.error("Failed to parse exercises", error);
+      }
+    } else if (Array.isArray(selectedExercisesString)) {
+      exercisesArray = selectedExercisesString;
+    }
+
+    // Map over the exercises array to add the sets property
+    return exercisesArray.map((exercise) => ({
+      ...exercise,
+      sets: [{ set: 1, kg: "", reps: "" }],
+    }));
+  });
+
+  const handleAddSet = (exerciseId) => {
+    setSelectedExercises((prevExercises) =>
+      prevExercises.map((exercise) =>
+        exercise.id === exerciseId
+          ? {
+              ...exercise,
+              sets: [
+                ...exercise.sets,
+                { set: exercise.sets.length + 1, kg: "", reps: "" },
+              ],
+            }
+          : exercise
+      )
+    );
+  };
+
+  const handleSetChange = (exerciseId, setIndex, field, value) => {
+    setSelectedExercises((prevExercises) =>
+      prevExercises.map((exercise) =>
+        exercise.id === exerciseId
+          ? {
+              ...exercise,
+              sets: exercise.sets.map((set, index) =>
+                index === setIndex ? { ...set, [field]: value } : set
+              ),
+            }
+          : exercise
+      )
+    );
+  };
 
   const addDayActivity = async () => {
     // if (!activityName) {
@@ -77,9 +130,6 @@ export default function CreateDayActivity() {
     });
   }, [navigation]);
 
-  // const handleNavigate = () => {
-  //   navigation.navigate("exerciseListScreen");
-  // };
   const handleNavigate = () => {
     router.push("exerciseListScreen");
   };
@@ -99,24 +149,60 @@ export default function CreateDayActivity() {
       </View>
       <View style={styles.separator} />
 
-      <View>
-        <FlatList
-          data={selectedExercises}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => {
-            return (
-              <View>
-                <Text style={styles.exerciseText}>Name: {item.name}</Text>
+      <FlatList
+        data={selectedExercises}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.exerciseContainer}>
+            <Text style={styles.exerciseName}>{item.name}</Text>
+            <Text style={styles.exerciseText}>Equipment: {item.equipment}</Text>
+
+            <View style={styles.tableHeader}>
+              <Text style={styles.columnText}>SET</Text>
+              <Text style={styles.columnText}>KG</Text>
+              <Text style={styles.columnText}>REPS</Text>
+            </View>
+
+            {item.sets.map((set, index) => (
+              <View style={styles.inputRow} key={index}>
+                <Text style={styles.setNumber}>{set.set}</Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder="KG"
+                  placeholderTextColor={Colors.gray}
+                  keyboardType="numeric"
+                  value={set.kg}
+                  onChangeText={(value) =>
+                    handleSetChange(item.id, index, "kg", value)
+                  }
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Reps"
+                  placeholderTextColor={Colors.gray}
+                  keyboardType="numeric"
+                  value={set.reps}
+                  onChangeText={(value) =>
+                    handleSetChange(item.id, index, "reps", value)
+                  }
+                />
               </View>
-            );
-          }}
-          ListEmptyComponent={
-            <Text style={styles.noExercisesText}>
-              Start adding an Exercise to the Day Activity.
-            </Text>
-          }
-        />
-      </View>
+            ))}
+
+            <ButtonPrimary
+              title="+ Add Set"
+              onPress={() => handleAddSet(item.id)}
+            />
+
+            <View style={styles.separator} />
+          </View>
+        )}
+        ListEmptyComponent={
+          <Text style={styles.noExercisesText}>
+            Start adding an Exercise to the Day Activity.
+          </Text>
+        }
+      />
 
       <ButtonSecondary title="Add Exercise" onPress={handleNavigate} />
     </View>
@@ -139,8 +225,16 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.gray,
     marginVertical: 10,
   },
+  exerciseName: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
   exerciseText: {
     color: "white",
+    fontSize: 18,
+    marginBottom: 5,
   },
   noExercisesText: {
     color: Colors.gray,
@@ -148,16 +242,40 @@ const styles = StyleSheet.create({
     fontSize: 18,
     marginTop: 20,
   },
-  saveButton: {
-    backgroundColor: Colors.primary,
-    padding: 15,
-    borderRadius: 5,
-    alignItems: "center",
-    marginTop: 20,
+  exerciseContainer: {
+    marginBottom: 20,
   },
-  saveButtonText: {
-    color: "#fff",
-    fontSize: 18,
+  tableHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginVertical: 5,
+  },
+  columnText: {
+    color: Colors.gray,
     fontWeight: "bold",
+    flex: 1,
+    textAlign: "center",
+  },
+  inputRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingBottom: 5,
+  },
+  setNumber: {
+    color: "white",
+    flex: 1,
+    textAlign: "center",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  input: {
+    flex: 1,
+    backgroundColor: Colors.darkGray,
+    color: "white",
+    borderRadius: 5,
+    padding: 10,
+    textAlign: "center",
+    marginHorizontal: 5,
   },
 });
